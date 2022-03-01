6ea9da2929e448dbef0c3bac4f348ad66888aa08
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2022-03-01 11:58:45 +0100

    Config example, initial commit

---
+++ FunctionApp/GetSomethingFunctions.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+
+namespace FunctionApp;
+
+public class GetSomethingFunctions
+{
+    private readonly Configuration _configuration;
+
+    public GetSomethingFunctions(Configuration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [FunctionName(nameof(GetLocalTime))]
+    public ActionResult GetLocalTime([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "localtime")] HttpRequest request)
+    {
+        return new OkObjectResult($"{_configuration.Header}: {DateTime.Now}");
+    }
+
+    public class Configuration
+    {
+        public string Header { get; set; }
+    }
+}
---
+++ FunctionApp/Startup.cs
@@ -0,0 +1,22 @@
+using FunctionApp;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly:FunctionsStartup(typeof(Startup))]
+
+namespace FunctionApp;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddConfiguration<GetSomethingFunctions.Configuration>();
+    }
+}
+
+public static class ConfigurationRegistrations
+{
+    public static IServiceCollection AddConfiguration<T>(this IServiceCollection services) where T : class =>
+        services.AddSingleton(provider => provider.GetService<IConfiguration>().Get<T>());
+}
