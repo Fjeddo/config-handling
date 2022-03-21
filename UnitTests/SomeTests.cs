b55817283c5443f4cfe2929d3ba67cf384f6cc64
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2022-03-21 09:13:49 +0100

    Add unit tests examples

---
+++ UnitTests/SomeTests.cs
@@ -0,0 +1,46 @@
+﻿using FunctionApp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace UnitTests
+{
+    public class SomeTests
+    {
+        private static readonly DefaultHttpRequest HttpRequest = new(new DefaultHttpContext());
+
+        [Fact]
+        public void Test_injecting_iconfiguration()
+        {
+            // Arrange
+            var configuration = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string>()
+            {
+                {"Header", "This is a header"}
+            }).Build();
+            
+            var sut = new OriginalTimeFunctions(configuration);
+
+            // Act
+            sut.GetLocalTimeOriginal(HttpRequest);
+
+            // Assert
+            //...
+        }
+
+        [Fact]
+        public void Test_injecting_poco_configuration()
+        {
+            // Arrange
+            var configuration = new AlternativeTimeFunctions.Configuration {Header = "This is a header"};
+
+            var sut = new AlternativeTimeFunctions(configuration);
+
+            // Act
+            sut.AltGetLocalTime(HttpRequest);
+
+            //Assert
+            //...
+        }
+    }
+}
